67146dd0b8b58698feb81a941d2aff5b65dd1610
LuizLimaDev <devluizlima@gmail.com>
2024-05-20 23:59:52 -0300

    feat: MenuMobile Sheet

--- app/_components/MenuMobile/MenuMobile.tsx
+++ app/_components/MenuMobile/MenuMobile.tsx
@@ -1,3 +1,11 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/app/_components/ui/sheet";
 import Image from "next/image";
 
 const MenuMobile = () => {
@@ -11,13 +19,58 @@ const MenuMobile = () => {
         className="drop-shadow-lg"
       />
 
-      <Image
-        src="/menu-hamburger.svg"
-        alt="logo imobiliária vigor"
-        width={17}
-        height={8}
-        className="drop-shadow-lg"
-      />
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="/menu-hamburger.svg"
+            alt="logo imobiliária vigor"
+            width={17}
+            height={8}
+            className="drop-shadow-lg"
+          />
+        </SheetTrigger>
+
+        <SheetContent>
+          <SheetHeader className="w-[100.2%] -translate-x-[.3%]">
+            <SheetTitle className="flex flex-col items-center justify-center bg-VIsecondary-color py-4">
+              <Image
+                src="/logo-blue.png"
+                alt="logo imobiliária vigor"
+                width={101}
+                height={46}
+                className="drop-shadow-lg"
+              />
+            </SheetTitle>
+          </SheetHeader>
+
+          <SheetDescription className="py-10 pl-10">
+            <ul>
+              <li className="mb-[3.125rem] text-left font-archivo font-bold uppercase text-VIprimary-color drop-shadow-sm">
+                Home
+              </li>
+              <li className="mb-[3.125rem] text-left font-archivo font-bold uppercase text-VIprimary-color drop-shadow-sm">
+                Sobre nós
+              </li>
+              <li className="mb-[3.125rem] text-left font-archivo font-bold uppercase text-VIprimary-color drop-shadow-sm">
+                Cadastre imóveis
+              </li>
+              <li className="mb-[3.125rem] text-left font-archivo font-bold uppercase text-VIprimary-color drop-shadow-sm">
+                Contato
+              </li>
+              <li className="mb-[3.125rem] text-left font-archivo font-bold uppercase text-VIprimary-color drop-shadow-sm">
+                Corretores
+              </li>
+              <li className="mb-[3.125rem] text-left font-archivo font-bold uppercase text-VIprimary-color drop-shadow-sm">
+                Todos imóveis
+              </li>
+            </ul>
+          </SheetDescription>
+
+          <div className="absolute -left-[0.3%] bottom-0 w-[100.2%] bg-VIprimary-color py-5 pl-3 text-VIligth-color">
+            <p>footer</p>
+          </div>
+        </SheetContent>
+      </Sheet>
     </div>
   );
 };
