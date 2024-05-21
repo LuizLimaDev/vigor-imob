e3fa43df3cfd7ed6809f097c48183730bce13572
LuizLimaDev <devluizlima@gmail.com>
2024-05-21 20:25:56 -0300

    fix: moving HeaderMenu & Footer to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,19 @@
 import FeaturedVenture from "./_components/FeaturedVenture/FeaturedVenture";
-import MenuMobile from "./_components/MenuMobile/MenuMobile";
 import VideoPlayer from "./_components/VideoPlayer/VideoPlayer";
 import SectionTitleBar from "./_components/SectionTitleBar/SectionTitleBar";
 import Feed from "./_components/Feed/Feed";
-import Footer from "./_components/Footer/Footer";
 
 export default function Home() {
   return (
     <>
       <main className="flex w-screen flex-col items-center justify-between bg-VIligth-color">
         <VideoPlayer />
-        <MenuMobile />
 
         <FeaturedVenture />
 
         <SectionTitleBar>Novidades</SectionTitleBar>
         <Feed />
       </main>
-      <Footer />
     </>
   );
 }
