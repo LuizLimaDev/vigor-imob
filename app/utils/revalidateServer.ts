cafaffe640613c46963c393b1a11002e5a091a6d
LuizLimaDev <devluizlime@gmail.com>
2024-07-03 23:03:27 -0300

    chore: clean up

--- app/utils/revalidateServer.ts
+++
@@ -1,8 +0,0 @@
-import { revalidatePath } from "next/cache";
-
-const revalidateServer = (path: string) => {
-  console.log(path, "page");
-  revalidatePath(path, "page");
-};
-
-export default revalidateServer;
