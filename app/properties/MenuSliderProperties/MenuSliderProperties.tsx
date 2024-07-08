5cd8d5761c78926eb8037b308bd51a9994b6a326
LuizLimaDev <devluizlime@gmail.com>
2024-07-08 19:10:23 -0300

    fix: properties page menu slide active

--- app/properties/MenuSliderProperties/MenuSliderProperties.tsx
+++ app/properties/MenuSliderProperties/MenuSliderProperties.tsx
@@ -11,14 +11,25 @@ import {
 } from "@/app/_components/ui/select";
 import { filterProperties, filterPropertiesCity } from "@/app/action/actions";
 
-const MenuSliderProperties = ({ filterButtons, cities }: any) => {
+const MenuSliderProperties = ({ filterButtons, cities, searchParams }: any) => {
+  const filter = searchParams.split("=")[1];
+
+  function handleClick(item: string) {
+    if (filter === item) {
+      filterProperties([""]);
+      return;
+    }
+
+    filterProperties([item]);
+  }
+
   return (
     <div className="my-8 ml-5 flex gap-4 overflow-scroll desktop:z-50 desktop:my-16 desktop:items-center desktop:justify-center [&::-webkit-scrollbar]:hidden">
       {filterButtons.map((item: string, index: number) => (
         <p
           key={index}
-          className="flex h-[2.5rem] flex-col items-center justify-center rounded-[.5rem] border-[.0625rem] border-VIprimary-color p-3 font-archivo text-sm font-bold capitalize text-VIprimary-color hover:border-VIsecondary-color desktop:hover:text-VIsecondary-color"
-          onClick={() => filterProperties([item])}
+          className={`${item === filter && "!border-VIprimary-color bg-VIprimary-color !text-VIligth-color"} flex h-[2.5rem] flex-col items-center justify-center rounded-[.5rem] border-[.0625rem] border-VIprimary-color p-3 font-archivo text-sm font-bold capitalize text-VIprimary-color hover:border-VIsecondary-color desktop:hover:text-VIsecondary-color `}
+          onClick={() => handleClick(item)}
         >
           {item}
         </p>
